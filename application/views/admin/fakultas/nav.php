3434d04eca822d045cb9d218a7b848a192626f42
agungdh <agungdh@live.com>
2018-05-03 08:19:08 +0700

    buat modul fakultas

---
+++ application/views/admin/fakultas/nav.php
@@ -0,0 +1,10 @@
+<li class="m-nav__separator">
+	-
+</li>
+<li class="m-nav__item">
+	<a href="<?php echo base_url('fakultas'); ?>" class="m-nav__link" data-toggle="m-tooltip" title="">
+		<span class="m-nav__link-text">
+			Fakultas
+		</span>
+	</a>
+</li>
