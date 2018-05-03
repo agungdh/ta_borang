6e389574b9cb6a18ade2fc62cea1ff3739f2ab43
agungdh <agungdh@live.com>
2018-05-03 08:17:04 +0700

    ganti sess save

--- application/config/config.php
+++ application/config/config.php
@@ -329,7 +329,7 @@ $config['cache_query_string'] = FALSE;
 | https://codeigniter.com/user_guide/libraries/encryption.html
 |
 */
-$config['encryption_key'] = 'adepkl';
+$config['encryption_key'] = null;
 
 /*
 |--------------------------------------------------------------------------
@@ -382,10 +382,10 @@ $config['encryption_key'] = 'adepkl';
 | except for 'cookie_prefix' and 'cookie_httponly', which are ignored here.
 |
 */
-$config['sess_driver'] = 'database';
+$config['sess_driver'] = 'files';
 $config['sess_cookie_name'] = 'ci_session';
 $config['sess_expiration'] = 7200;
-$config['sess_save_path'] = 'ci_sessions';
+$config['sess_save_path'] = null;
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
