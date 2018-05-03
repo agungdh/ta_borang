7a27b2ad78b74e0a8602c64756ff4984a32eec48
agungdh <agungdh@live.com>
2018-05-03 12:19:03 +0700

    tambah base_url

--- application/views/admin/fakultas/index.php
+++ application/views/admin/fakultas/index.php
@@ -60,7 +60,7 @@
 						<tr>
 							<td><?php echo $item->nama; ?></td>
 							<td>
-								<a href="#" class="m-portlet__nav-link btn m-btn m-btn--hover-accent m-btn--icon m-btn--icon-only m-btn--pill" data-toggle="m-tooltip" title="Prodi">
+								<a href="<?php echo base_url('admin/prodi/' .  $item->id); ?>" class="m-portlet__nav-link btn m-btn m-btn--hover-accent m-btn--icon m-btn--icon-only m-btn--pill" data-toggle="m-tooltip" title="Prodi">
 									<i class="la la-external-link"></i>
 								</a>
 								<a href="<?php echo base_url('admin/fakultas/ubah/' . $item->id); ?>" class="m-portlet__nav-link btn m-btn m-btn--hover-accent m-btn--icon m-btn--icon-only m-btn--pill" data-toggle="m-tooltip" title="Ubah">
