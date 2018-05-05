471dc4b02ba76c4aabf6ab2b0a58fe91e4c94635
agungdh <agungdh@live.com>
2018-05-05 07:11:16 +0700

    update modul profil

--- application/controllers/Profil.php
+++ application/controllers/Profil.php
@@ -2,16 +2,64 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Profil extends CI_Controller {
+	var $table;
+
 	function __construct(){
 		parent::__construct();
+
+		$this->table = "user";
 	}
 
 	function index() {
 		$data['nav'] = "profil/nav";
 		$data['isi'] = "profil/index";
-		$data['data']['user'] = $this->db->get_where('user', ['id' => $this->session->id])->row();
+		$data['data']['user'] = $this->db->get_where($this->table, ['id' => $this->session->id])->row();
 		
 		$this->load->view("template/template", $data);
+	}
+
+	function ubah() {
+		foreach ($this->input->post('data') as $key => $value) {
+			$data[$key] = $value;
+		}
+
+		foreach ($this->input->post('where') as $key => $value) {
+			$where[$key] = $value;
+		}
+
+		$this->db->update($this->table, $data, $where);
+
+		$this->session->set_userdata('nama', $data['nama']);
+
+		$flashdata['header'] = "BERHASIL !!!";
+		$flashdata['pesan'] = "Berhasil Ubah Profil !!!";
+		$flashdata['status'] = "success";
+		
+		$this->session->set_flashdata('data', $flashdata);
+		redirect(base_url('profil'));
+	}
+
+	function ganti_password() {
+		foreach ($this->input->post('data') as $key => $value) {
+			if ($key == "password") {
+				$data[$key] = hash("sha512", $value);
+			} else {
+				$data[$key] = $value;
+			}
+		}
+
+		foreach ($this->input->post('where') as $key => $value) {
+			$where[$key] = $value;
+		}
+
+		$this->db->update($this->table, $data, $where);
+
+		$flashdata['header'] = "BERHASIL !!!";
+		$flashdata['pesan'] = "Berhasil Ganti Password !!!";
+		$flashdata['status'] = "success";
+		
+		$this->session->set_flashdata('data', $flashdata);
+		redirect(base_url('profil'));
 	}
 
 }
--- application/views/profil/index.php
+++ application/views/profil/index.php
@@ -14,7 +14,7 @@
 			</div>
 		</div>
 		<!--begin::Form-->
-		<form class="m-form m-form--fit m-form--label-align-right" method="post" action="<?php echo base_url('profil/aksi_ubah'); ?>">
+		<form class="m-form m-form--fit m-form--label-align-right" method="post" action="<?php echo base_url('profil/ubah'); ?>">
 			<div class="m-portlet__body">
 				<div>
 					<input type="hidden" name="where[id]" value="<?php echo isset($data['user']) ? $data['user']->id : null; ?>">
@@ -82,7 +82,7 @@
 			<div class="m-portlet__foot m-portlet__foot--fit">
 				<div class="m-form__actions">
 					<input type="submit" class="btn btn-primary" value="Simpan">
-					<a href="<?php echo base_url('admin/fakultas'); ?>" class="btn btn-secondary">
+					<a href="<?php echo base_url(''); ?>" class="btn btn-secondary">
 						Kembali
 					</a>
 				</div>
@@ -92,3 +92,70 @@
 	</div>
 	<!--end::Portlet-->
 </div>
+
+<div class="col-md-12">
+	<!--begin::Portlet-->
+	<div class="m-portlet m-portlet--tab">
+		<div class="m-portlet__head">
+			<div class="m-portlet__head-caption">
+				<div class="m-portlet__head-title">
+					<span class="m-portlet__head-icon m--hide">
+						<i class="la la-gear"></i>
+					</span>
+					<h3 class="m-portlet__head-text">
+						Ganti Password
+					</h3>
+				</div>
+			</div>
+		</div>
+		<!--begin::Form-->
+		<form class="m-form m-form--fit m-form--label-align-right" id="form_ganti_password" method="post" action="<?php echo base_url('profil/ganti_password'); ?>">
+			<div class="m-portlet__body">
+				<div>
+					<input type="hidden" name="where[id]" value="<?php echo isset($data['user']) ? $data['user']->id : null; ?>">
+				</div>
+
+				<div class="form-group m-form__group">
+					<label for="password">
+						Password
+					</label>
+					<input required class="form-control m-input" type="password" id="password" name="data[password]">
+				</div>
+				<div class="form-group m-form__group">
+					<label for="password2">
+						Ulangi Password
+					</label>
+					<input required class="form-control m-input" type="password" id="password2" name="cdata[password2]">
+				</div>
+			</div>
+			<div class="m-portlet__foot m-portlet__foot--fit">
+				<div class="m-form__actions">
+					<input type="submit" class="btn btn-primary" value="Simpan">
+					<a href="<?php echo base_url(''); ?>" class="btn btn-secondary">
+						Kembali
+					</a>
+				</div>
+			</div>
+		</form>
+		<!--end::Form-->
+	</div>
+	<!--end::Portlet-->
+</div>
+
+<?php
+$flashdata = $this->session->flashdata('data');
+
+if ($flashdata != null) {
+	$this->pustaka->swal3($flashdata['header'], $flashdata['pesan'], $flashdata['status']);
+}
+?>
+
+<script type="text/javascript">
+$("#form_ganti_password").on("submit", function(){
+	if ($("#password").val() != $("#password2").val()) {
+		swal("ERROR !!!", "Password Tidak Sama !!!", "error");
+
+		return false;
+	}
+})
+</script>
