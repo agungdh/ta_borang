da67415e1a14bba1ee4e79c5e8b7974ed76da7c6
agungdh <agungdh@live.com>
2018-05-05 12:17:55 +0700

    tambah menu unit

--- application/views/template/menu_admin.php
+++ application/views/template/menu_admin.php
@@ -10,3 +10,16 @@
 		</span>
 	</a>
 </li>
+
+<li class="m-menu__item " aria-haspopup="true" >
+	<a  href="<?php echo base_url('admin/unit'); ?>" class="m-menu__link ">
+		<i class="m-menu__link-icon flaticon-line-graph"></i>
+		<span class="m-menu__link-title">
+			<span class="m-menu__link-wrap">
+				<span class="m-menu__link-text">
+					Unit
+				</span>
+			</span>
+		</span>
+	</a>
+</li>
