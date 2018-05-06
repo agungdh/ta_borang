12bf4d0191a29ce64300291774fc8c2462e331c4
agungdh <agungdh@live.com>
2018-05-06 15:41:04 +0700

    update modul borang
    ganti tooltip

--- application/views/admin/borang/index.php
+++ application/views/admin/borang/index.php
@@ -64,7 +64,7 @@
 							<td><?php echo $item->nama; ?></td>
 							<td><?php echo $item->tahun; ?></td>
 							<td>
-								<a href="<?php echo base_url('admin/tipeborang/index/' .  $item->id); ?>" class="m-portlet__nav-link btn m-btn m-btn--hover-accent m-btn--icon m-btn--icon-only m-btn--pill" data-toggle="m-tooltip" title="Prodi">
+								<a href="<?php echo base_url('admin/tipeborang/index/' .  $item->id); ?>" class="m-portlet__nav-link btn m-btn m-btn--hover-accent m-btn--icon m-btn--icon-only m-btn--pill" data-toggle="m-tooltip" title="Tipe Borang">
 									<i class="la la-external-link"></i>
 								</a>
 								<a href="<?php echo base_url('admin/borang/ubah/' . $item->id); ?>" class="m-portlet__nav-link btn m-btn m-btn--hover-accent m-btn--icon m-btn--icon-only m-btn--pill" data-toggle="m-tooltip" title="Ubah">
