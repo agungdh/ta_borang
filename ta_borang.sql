c538f1e4041896020fa8135da413e5ad600cb02a
agungdh <agungdh@live.com>
2018-05-08 20:18:01 +0700

    update db

--- ta_borang.sql
+++ ta_borang.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: May 03, 2018 at 06:03 AM
+-- Generation Time: May 08, 2018 at 08:16 PM
 -- Server version: 10.1.29-MariaDB-6
--- PHP Version: 7.2.4-1+ubuntu18.04.1+deb.sury.org+1
+-- PHP Version: 7.2.5-1+ubuntu18.04.1+deb.sury.org+1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -48,6 +48,14 @@ CREATE TABLE `butir` (
   `substandar_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `butir`
+--
+
+INSERT INTO `butir` (`id`, `nomor`, `nama`, `substandar_id`) VALUES
+(4, '1.1.4', 'satu satu satu jadi empat test', 2),
+(5, '1.1.2', 'satu satu dua', 2);
+
 -- --------------------------------------------------------
 
 --
@@ -58,6 +66,14 @@ CREATE TABLE `fakultas` (
   `id` int(11) NOT NULL,
   `nama` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `fakultas`
+--
+
+INSERT INTO `fakultas` (`id`, `nama`) VALUES
+(4, 'Fakultas Teknik Informatika'),
+(5, 'Fakultas Ekonomi');
 
 -- --------------------------------------------------------
 
@@ -72,6 +88,18 @@ CREATE TABLE `listdokumen` (
   `tipe_listdokumen_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `listdokumen`
+--
+
+INSERT INTO `listdokumen` (`id`, `keterangan`, `butir_id`, `tipe_listdokumen_id`) VALUES
+(1, 'ini iseng', 4, 4),
+(2, 'test', 4, 6),
+(3, 'file satu', 5, 4),
+(5, 'file tiga', 5, 5),
+(6, 'file dua', 5, 5),
+(7, 'test aja', 5, 4);
+
 -- --------------------------------------------------------
 
 --
@@ -80,10 +108,18 @@ CREATE TABLE `listdokumen` (
 
 CREATE TABLE `pengajuan` (
   `id` int(11) NOT NULL,
+  `tanggal_pengajuan` date NOT NULL,
   `tahun_borang` year(4) NOT NULL,
   `unit_id` int(11) NOT NULL,
   `tipeversi_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `pengajuan`
+--
+
+INSERT INTO `pengajuan` (`id`, `tanggal_pengajuan`, `tahun_borang`, `unit_id`, `tipeversi_id`) VALUES
+(2, '2018-05-08', 2011, 7, 5);
 
 -- --------------------------------------------------------
 
@@ -96,6 +132,16 @@ CREATE TABLE `prodi` (
   `nama` varchar(255) NOT NULL,
   `fakultas_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `prodi`
+--
+
+INSERT INTO `prodi` (`id`, `nama`, `fakultas_id`) VALUES
+(13, 'fti 1', 4),
+(14, 'fti 2', 4),
+(15, 'fe 3', 5),
+(16, 'fe 4', 5);
 
 -- --------------------------------------------------------
 
@@ -110,6 +156,15 @@ CREATE TABLE `standar` (
   `tipeversi_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `standar`
+--
+
+INSERT INTO `standar` (`id`, `nomor`, `nama`, `tipeversi_id`) VALUES
+(2, '1', 'satu satu aja', 5),
+(4, '3', 'asf asfewf adsf dua jadi 3', 5),
+(5, '2', 'test', 5);
+
 -- --------------------------------------------------------
 
 --
@@ -123,6 +178,13 @@ CREATE TABLE `substandar` (
   `standar_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `substandar`
+--
+
+INSERT INTO `substandar` (`id`, `nomor`, `nama`, `standar_id`) VALUES
+(2, '1.5', 'satu empat jadi lima', 4);
+
 -- --------------------------------------------------------
 
 --
@@ -135,6 +197,14 @@ CREATE TABLE `tipeversi` (
   `versi_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `tipeversi`
+--
+
+INSERT INTO `tipeversi` (`id`, `tipe`, `versi_id`) VALUES
+(3, 'test 1', 3),
+(5, 'qwe 1', 4);
+
 -- --------------------------------------------------------
 
 --
@@ -145,6 +215,15 @@ CREATE TABLE `tipe_listdokumen` (
   `id` int(11) NOT NULL,
   `tipe` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tipe_listdokumen`
+--
+
+INSERT INTO `tipe_listdokumen` (`id`, `tipe`) VALUES
+(4, 'Dokumen Wajib'),
+(5, 'Dokumen Pendukung'),
+(6, 'Dokumen Visitasi');
 
 -- --------------------------------------------------------
 
@@ -163,7 +242,11 @@ CREATE TABLE `unit` (
 --
 
 INSERT INTO `unit` (`id`, `unit`, `prodi_id`) VALUES
-(1, 1, NULL);
+(1, 1, NULL),
+(7, 2, 13),
+(8, 2, 14),
+(9, 2, 15),
+(10, 2, 16);
 
 -- --------------------------------------------------------
 
@@ -180,6 +263,14 @@ CREATE TABLE `user` (
   `unit_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `nama`, `username`, `password`, `level`, `unit_id`) VALUES
+(1, 'Administrator', 'admin', 'c7ad44cbad762a5da0a452f9e854fdc1e0e7a52a38015f23f3eab1d80b931dd472634dfac71cd34ebc35d16ab7fb8a90c81f975113d6c7538dc69dd8de9077ec', 1, NULL),
+(2, 'Operator FTI 1', 'opfti1', 'd404559f602eab6fd602ac7680dacbfaadd13630335e951f097af3900e9de176b6db28512f2e000b9d04fba5133e8b1c6e8df59db3a8ab9d60be4b97cc9e81db', 3, 7);
+
 -- --------------------------------------------------------
 
 --
@@ -191,6 +282,45 @@ CREATE TABLE `versi` (
   `nama` varchar(255) NOT NULL,
   `tahun` year(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `versi`
+--
+
+INSERT INTO `versi` (`id`, `nama`, `tahun`) VALUES
+(3, '9 standar', 2018),
+(4, '7 standar', 2009);
+
+-- --------------------------------------------------------
+
+--
+-- Stand-in structure for view `v_listdokumen`
+-- (See below for the actual view)
+--
+CREATE TABLE `v_listdokumen` (
+`id` int(11)
+,`keterangan` varchar(255)
+,`butir_id` int(11)
+,`tipe_listdokumen_id` int(11)
+,`nomor_butir` varchar(255)
+,`nama_butir` varchar(255)
+,`substandar_id` int(11)
+,`nomor_substandar` varchar(255)
+,`nama_substandar` varchar(255)
+,`standar_id` int(11)
+,`nomor_standar` varchar(255)
+,`nama_standar` varchar(255)
+,`tipeversi_id` int(11)
+);
+
+-- --------------------------------------------------------
+
+--
+-- Structure for view `v_listdokumen`
+--
+DROP TABLE IF EXISTS `v_listdokumen`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `v_listdokumen`  AS  select `l`.`id` AS `id`,`l`.`keterangan` AS `keterangan`,`l`.`butir_id` AS `butir_id`,`l`.`tipe_listdokumen_id` AS `tipe_listdokumen_id`,`b`.`nomor` AS `nomor_butir`,`b`.`nama` AS `nama_butir`,`b`.`substandar_id` AS `substandar_id`,`ss`.`nomor` AS `nomor_substandar`,`ss`.`nama` AS `nama_substandar`,`ss`.`standar_id` AS `standar_id`,`s`.`nomor` AS `nomor_standar`,`s`.`nama` AS `nama_standar`,`s`.`tipeversi_id` AS `tipeversi_id` from (((`standar` `s` join `substandar` `ss`) join `butir` `b`) join `listdokumen` `l`) where ((`l`.`butir_id` = `b`.`id`) and (`b`.`substandar_id` = `ss`.`id`) and (`ss`.`standar_id` = `s`.`id`)) ;
 
 --
 -- Indexes for dumped tables
@@ -301,73 +431,73 @@ ALTER TABLE `berkas`
 -- AUTO_INCREMENT for table `butir`
 --
 ALTER TABLE `butir`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `fakultas`
 --
 ALTER TABLE `fakultas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `listdokumen`
 --
 ALTER TABLE `listdokumen`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `pengajuan`
 --
 ALTER TABLE `pengajuan`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `prodi`
 --
 ALTER TABLE `prodi`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `standar`
 --
 ALTER TABLE `standar`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `substandar`
 --
 ALTER TABLE `substandar`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `tipeversi`
 --
 ALTER TABLE `tipeversi`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `tipe_listdokumen`
 --
 ALTER TABLE `tipe_listdokumen`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `unit`
 --
 ALTER TABLE `unit`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `versi`
 --
 ALTER TABLE `versi`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Constraints for dumped tables
