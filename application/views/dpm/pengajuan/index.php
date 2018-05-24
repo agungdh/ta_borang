fa208d2b990702404aaeb63f7611cd8a6bc2e033
agungdh <agungdh@live.com>
2018-05-24 11:11:43 +0700

    ubah modul pengajuah

--- application/views/dpm/pengajuan/index.php
+++ application/views/dpm/pengajuan/index.php
@@ -27,17 +27,6 @@
 							</div>
 						</div>
 					</div>
-					<div class="col-xl-4 order-1 order-xl-2 m--align-right">
-						<a href="<?php echo base_url('dpm/pengajuan/tambah'); ?>" class="btn btn-accent m-btn m-btn--custom m-btn--icon m-btn--air m-btn--pill">
-							<span>
-								<i class="la la-plus"></i>
-								<span>
-									Pengajuan
-								</span>
-							</span>
-						</a>
-						<div class="m-separator m-separator--dashed d-xl-none"></div>
-					</div>
 				</div>
 			</div>
 			<!--end: Search Form -->
