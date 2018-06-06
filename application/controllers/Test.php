cfc304c291bcdd0f46fc8ba947ddd615f9cada05
agungdh <agungdh@live.com>
2018-06-06 20:35:44 +0700

    update

--- application/controllers/Test.php
+++ application/controllers/Test.php
@@ -9,9 +9,55 @@ class Test extends CI_Controller {
 	}
 
 	function index() {
-		foreach ($this->db->get_where('standar', ['versi_id' => 1])->result() as $item) {
-			var_dump($item);
-		}
+		?>
+		<!DOCTYPE html>
+		<html>
+		<head>
+			<title></title>
+		</head>
+		<body>
+			<table border="1">
+				<thead>
+					<tr>
+						<th>Standar</th>
+						<th>Substandar</th>
+						<th>Butir</th>
+					</tr>
+				</thead>
+				<tbody>
+					<?php
+					$standar = $this->db->get_where('standar', ['versi_id' => 1])->result();
+					foreach ($standar as $item_standar) {
+						$substandar = $this->db->get_where('substandar', ['standar_id' => $item_standar->id])->result();
+						foreach ($substandar as $item_substandar) {
+							$butir = $this->db->get_where('butir', ['substandar_id' => $item_substandar->id])->result();
+							if ($butir != null) {
+								foreach ($butir as $item_butir) {
+									?>
+									<tr>
+										<td><?php echo $item_standar->nomor . ' ' . $item_standar->nama; ?></td>
+										<td><?php echo $item_substandar->nomor . ' ' . $item_substandar->nama; ?></td>
+										<td><?php echo $item_butir->nomor . ' ' . $item_butir->nama; ?></td>
+									</tr>
+									<?php
+								}
+							} else {
+								?>
+								<tr>
+									<td><?php echo $item_standar->nomor . ' ' . $item_standar->nama; ?></td>
+									<td><?php echo $item_substandar->nomor . ' ' . $item_substandar->nama; ?></td>
+									<td></td>
+								</tr>
+								<?php
+							}
+						}
+					}
+					?>			
+				</tbody>
+			</table>		
+		</body>
+		</html>
+		<?php
 	}
 
 }
