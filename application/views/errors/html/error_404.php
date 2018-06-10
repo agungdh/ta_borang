e11616084417930afe312f016953baed23114624
agungdh <agungdh@live.com>
2018-06-10 21:25:27 +0700

    error 404

--- application/views/errors/html/error_404.php
+++ application/views/errors/html/error_404.php
@@ -1,65 +1,64 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-
-$ci = new CI_Controller();
-$ci =& get_instance();
-
-$now = date('YmdHis');
-?>
-
-<!DOCTYPE html>
-<html lang="en" >
-
+?><!DOCTYPE html>
+<html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <title>404 Page Not Found</title>
-  
-  	    <!-- Favicon -->
-	    <?php
-	    if (file_exists('uploads/favicon')) {
-	      $favicon = 'uploads/favicon';
-	    } else {
-	      $favicon = 'assets/favicon.png';
-	    }
-	    ?>
-  	 	<link rel="shortcut icon" href="<?php echo base_url($favicon) . '?time=' . $now; ?>"/>
+<meta charset="utf-8">
+<title>404 Page Not Found</title>
+<style type="text/css">
 
-      <link rel="stylesheet" href="<?php echo base_url('assets/404/css/bootstrap.min.css'); ?>">
+::selection { background-color: #E13300; color: white; }
+::-moz-selection { background-color: #E13300; color: white; }
 
-      <link rel="stylesheet" href="<?php echo base_url('assets/404/css/style.css'); ?>">
+body {
+  background-color: #fff;
+  margin: 40px;
+  font: 13px/20px normal Helvetica, Arial, sans-serif;
+  color: #4F5155;
+}
 
-  
+a {
+  color: #003399;
+  background-color: transparent;
+  font-weight: normal;
+}
+
+h1 {
+  color: #444;
+  background-color: transparent;
+  border-bottom: 1px solid #D0D0D0;
+  font-size: 19px;
+  font-weight: normal;
+  margin: 0 0 14px 0;
+  padding: 14px 15px 10px 15px;
+}
+
+code {
+  font-family: Consolas, Monaco, Courier New, Courier, monospace;
+  font-size: 12px;
+  background-color: #f9f9f9;
+  border: 1px solid #D0D0D0;
+  color: #002166;
+  display: block;
+  margin: 14px 0 14px 0;
+  padding: 12px 10px 12px 10px;
+}
+
+#container {
+  margin: 10px;
+  border: 1px solid #D0D0D0;
+  box-shadow: 0 0 8px #D0D0D0;
+}
+
+p {
+  margin: 12px 15px 12px 15px;
+}
+</style>
 </head>
-
 <body>
-
-  <link href='https://fonts.googleapis.com/css?family=Anton|Passion+One|PT+Sans+Caption' rel='stylesheet' type='text/css'>
-<body>
-
-        <!-- Error Page -->
-            <div class="error">
-                <div class="container-floud">
-                    <div class="col-xs-12 ground-color text-center">
-                        <div class="container-error-404">
-                            <div class="clip"><div class="shadow"><span class="digit thirdDigit"></span></div></div>
-                            <div class="clip"><div class="shadow"><span class="digit secondDigit"></span></div></div>
-                            <div class="clip"><div class="shadow"><span class="digit firstDigit"></span></div></div>
-                            <div class="msg">OH!<span class="triangle"></span></div>
-                        </div>
-                        <h2 class="h1">Sorry! Page not found</h2>
-                    </div>
-                </div>
-            </div>
-        <!-- Error Page -->
+  <div id="container">
+    <h1><?php echo $heading; ?></h1>
+    <?php echo $message; ?>
+  </div>
 </body>
-  
-  
-
-    <script  src="<?php echo base_url('assets/404/js/index.js'); ?>"></script>
-
-
-
-
-</body>
-
 </html>
