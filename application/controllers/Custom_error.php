f43220cd790e681b02313ceda5ea09e452d53429
agungdh <agungdh@live.com>
2018-08-13 14:51:39 +0700

    update custom error

--- application/controllers/Custom_error.php
+++ application/controllers/Custom_error.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Error extends CI_Controller {
+class Custom_error extends CI_Controller {
 	function __construct(){
 		parent::__construct();
 	}
